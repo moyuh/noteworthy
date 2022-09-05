d049f6404a5f853446e22e0e8f569e4781d89d1d
moyuh <mlroth94@gmail..com>
2022-09-05 16:09:28 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+//call variables for required modules 
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const api = require('./routes/apiRoutes');
+const html = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`App Listening at http://localhost${PORT}🧨`))
