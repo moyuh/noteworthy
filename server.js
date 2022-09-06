995e5abd6fa4bc608b59ad56edd9bcd1443a0cd2
moyuh <mlroth94@gmail..com>
2022-09-05 19:05:38 -0600

    fixes notes html issues

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 //call variables for required modules 
+const PORT = process.env.PORT || 3001;
 const express = require('express');
+const app = express();
 const fs = require('fs');
 const path = require('path');
 const api = require('./routes/apiRoutes');
 const html = require('./routes/htmlRoutes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+
+
 
 //middleware
-app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.json());
 app.use('/api', api);
 app.use('/', html);
 
-app.use(express.static('public'));
+
 
 app.listen(PORT, () => {console.log(`App Listening at http://localhost:${PORT}`)});
